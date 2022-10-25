325c9ecc42bbb7bcbae3adbbf672e00c77e3b51f
luankefei <luankefei@gmail.com>
2022-10-25 14:07:18 +0800

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// comps
+import Layout from "@containers/Layout";
+import ErrorBoundary from "@containers/ErrorBoundray";
+// import OpenIMContainer from "@containers/OpenIMContainer";
+
+// styles
+import "../styles/globals.css";
+import "antd/dist/antd.css";
+import globalStyles from "../styles/globals";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const renderChildren = () => {
+    console.log("Component.name", Component.displayName);
+    if (Component.displayName === "Login") return <Component />;
+    return (
+      <Layout>
+        {/* <OpenIMContainer> */}
+        {/* <FetchContext.Provider> */}
+        <Component {...pageProps} />
+        {/* </FetchContext.Provider> */}
+        {/* </OpenIMContainer> */}
+      </Layout>
+    );
+  };
+  return (
+    <>
+      {globalStyles}
+      <Head>
+        <title>ingroup - your web3 address book</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ErrorBoundary>{renderChildren()}</ErrorBoundary>
+    </>
+  );
 }
-
-export default MyApp
